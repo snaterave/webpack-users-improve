c4cf3be5599807d0afa963ede86a1d413b62e327
snatera <snatera.ve@gmail.com>
2023-06-24 17:04:00 -0300

    Added config dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -73,7 +75,9 @@ module.exports = {
                   to: "assets/images"
               }
               ]
-          })
+          }),
+          new Dotenv(),
+          new CleanWebpackPlugin()    
       ],
       optimization: {
         minimize: true,
